6945e0d0e8c561a22947c746affb3d868ba14ff2
rauldangio <jojofagqwrt@gmail.com>
2023-04-05 08:44:39 -0300

    terminado

---
+++ src/main/java/raul/com/br/provacliente/ProvaclienteApplication.java
@@ -0,0 +1,13 @@
+package raul.com.br.provacliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaclienteApplication.class, args);
+	}
+
+}
